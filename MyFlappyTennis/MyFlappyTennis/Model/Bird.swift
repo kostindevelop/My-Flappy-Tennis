ee0c48464bc689cf8dccf7782ea92933e50f22f3
Konstantin Igorevich <kostindevelop@gmail.com>
2018-05-04 21:51:22 +0300

    add model classes

---
+++ MyFlappyTennis/MyFlappyTennis/Model/Bird.swift
@@ -0,0 +1,9 @@
+//
+//  Bird.swift
+//  MyFlappyTennis
+//
+//  Created by Konstantin Igorevich on 5/4/18.
+//  Copyright © 2018 Konstantin Igorevich. All rights reserved.
+//
+
+import Foundation
---
+++ MyFlappyTennis/MyFlappyTennis/Model/Sprite.swift
@@ -0,0 +1,9 @@
+//
+//  Sprite.swift
+//  MyFlappyTennis
+//
+//  Created by Konstantin Igorevich on 5/4/18.
+//  Copyright © 2018 Konstantin Igorevich. All rights reserved.
+//
+
+import Foundation
