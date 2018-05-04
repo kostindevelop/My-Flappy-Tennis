ef3977fb01a2dd512b860bba112d69b8f100858a
Konstantin Igorevich <kostindevelop@gmail.com>
2018-05-04 21:48:46 +0300

    initial commit

---
+++ MyFlappyTennis/MyFlappyTennis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFlappyTennis
+//
+//  Created by Konstantin Igorevich on 5/4/18.
+//  Copyright © 2018 Konstantin Igorevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
