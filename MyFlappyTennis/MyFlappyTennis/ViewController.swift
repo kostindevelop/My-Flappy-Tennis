5a60eb1cd68073fe5a4382dca378f1c07a3aeb0c
Konstantin Igorevich <kostindevelop@gmail.com>
2018-05-05 16:54:57 +0300

    add block and bird in viewController and func moving this elements

--- MyFlappyTennis/MyFlappyTennis/ViewController.swift
+++ MyFlappyTennis/MyFlappyTennis/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
+enum Direction {
+    case rigth
+    case left
+    case up
+    case down
+}
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    
+    var bird: Bird!
+    var rocket: Rocket!
+    var timer: Timer!
+    var direction: Direction = .rigth
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        runTimer()
     }
-
-
+    
+    func runTimer() {
+        bird = Bird.addBird(to: view)
+        rocket = Rocket.addRocket(to: view)
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { _ in
+            self.moveBird()
+            self.moveRocket()
+        })
+    }
+    func moveBird() {
+        if direction == .rigth && bird.rightSideX < self.view.frame.size.width  {
+            bird.transform = CGAffineTransform(scaleX: 1, y: 1)
+            bird.frame.origin.x += 1
+            if bird.rightSideX >= self.view.frame.size.width {
+                direction = .left
+            }
+        } else {
+            bird.transform = CGAffineTransform(scaleX: -1, y: 1)
+            bird.frame.origin.x -= 1
+            if bird.leftSideX < 0 {
+                direction = .rigth
+            }
+        }
+    }
+    func moveRocket() {
+        if direction == .down && rocket.downSide < self.view.frame.size.height  {
+            rocket.frame.origin.y += 1
+            if rocket.downSide >= self.view.frame.size.height {
+                direction = .up
+            }
+        } else {
+            bird.frame.origin.y -= 1
+            if rocket.upSide < 0 {
+                direction = .down
+            }
+        }
+    }
 }
 
