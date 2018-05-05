aacec5e3a966484728b30da08d4eef394e2f7402
Konstantin Igorevich <kostindevelop@gmail.com>
2018-05-05 16:51:29 +0300

    add func add Block in class Block

--- MyFlappyTennis/MyFlappyTennis/Model/Rocket.swift
+++ MyFlappyTennis/MyFlappyTennis/Model/Rocket.swift
@@ -9,5 +9,18 @@
 import UIKit
 
 class Rocket: UIView {
+    var downSide: CGFloat {
+        return self.frame.origin.y + self.frame.size.height
+    }
+    var upSide: CGFloat {
+        return self.frame.origin.y
+    }
     
+    static func addRocket(to view:UIView) -> Rocket {
+        let rect = CGRect(x: 20, y: 20, width: 10, height: 70)
+        let rocket = Rocket(frame: rect)
+        rocket.backgroundColor = .black
+        view.addSubview(rocket)
+        return rocket
+    }
 }
